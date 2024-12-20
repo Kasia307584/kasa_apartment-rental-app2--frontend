9501895540eee09c8c04c19bba0c3d571c0b76da
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-12-20 18:25:10 +0100

    ProductDetailsView component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import "./App.css";
-import Card from "./components/ProductCard.tsx";
-import Footer from "./components/FooterElement.tsx";
-import Dropdown from "./components/DropdownBtn.tsx";
-import Carousel from "./components/Carousel.tsx";
 import Layout from "./views/Layout.tsx";
 // import { Link } from "react-router-dom";
 
@@ -17,10 +13,6 @@ function App() {
         <p>A Propos</p>
         {/* </Link> */}
       </Layout>
-      <Carousel imageSrc=""></Carousel>
-      <Dropdown dropdownTitle="" dropdownContent=""></Dropdown>
-      <Card image="" title="" productId=""></Card>
-      <Footer></Footer>
     </>
   );
 }
---
+++ src/views/ProductDetailsView.tsx
@@ -0,0 +1,43 @@
+import { useParams } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import Carousel from "../components/Carousel";
+import Dropdown from "../components/DropdownBtn";
+
+function ProductDetailsView() {
+  const params = useParams();
+  const productId = params.productId;
+  // access the fetched data and find the product that matches the productId
+  return (
+    <>
+      <Carousel imageSrc="" />
+      <div className="product">
+        <h1 className="product__name">{}</h1>
+        <p className="product__location">{}</p>
+        <div className="product__tags">
+          {/*map through tags and display one by one*/}
+          <p className="product-tag">{}</p>
+        </div>
+      </div>
+      <div className="product-owner">
+        <p className="product-owner__name">{}</p>
+        <div>
+          <img src="" alt="Owner photo" />
+        </div>
+      </div>
+      <div className="stars">
+        {/*iterate over the data to apply conditionally colors on stars */}
+        <FontAwesomeIcon icon={faStar} className="star--recieved" />
+        <FontAwesomeIcon icon={faStar} className="star--recieved" />
+        <FontAwesomeIcon icon={faStar} className="star--recieved" />
+        <FontAwesomeIcon icon={faStar} className="star--nonrecieved" />
+        <FontAwesomeIcon icon={faStar} className="star--nonrecieved" />
+      </div>
+      <div className="product__descriptions">
+        {/*put the value of dropdownContent dinamically based on data */}
+        <Dropdown dropdownTitle="Description" dropdownContent="" />
+        <Dropdown dropdownTitle="Équipements" dropdownContent="" />
+      </div>
+    </>
+  );
+}
