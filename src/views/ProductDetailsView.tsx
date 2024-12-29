248b104b2a55c4378c053cc448b2253a161b4412
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2024-12-29 21:31:04 +0100

    GraphQL query modification in ProductDetailsView component

--- src/views/ProductDetailsView.tsx
+++ src/views/ProductDetailsView.tsx
@@ -6,11 +6,9 @@ import Dropdown from "../components/DropdownBtn";
 import { gql, useQuery } from "@apollo/client";
 
 const GET_APARTMENTS = gql`
-  query GetApartments {
-    apartments {
-      id
+  query GetApartments($id: ID!) {
+    apartment(id: $id) {
       title
-      cover
       pictures
       description
       host {
@@ -26,48 +24,43 @@ const GET_APARTMENTS = gql`
 `;
 
 interface Product {
-  id: string;
-  title: string;
-  cover: string;
-  pictures: string[];
-  description: string;
-  host: {
-    name: string;
-    picture: string;
+  apartment: {
+    title: string;
+    pictures: string[];
+    description: string;
+    host: {
+      name: string;
+      picture: string;
+    };
+    rating: string;
+    location: string;
+    equipments: string[];
+    tags: string[];
   };
-  rating: string;
-  location: string;
-  equipments: string[];
-  tags: string[];
-}
-
-interface Products {
-  apartments: Product[];
 }
 
 function ProductDetailsView() {
   const params = useParams();
   const productId = params.productId;
 
-  const { loading, error, data } = useQuery<Products>(GET_APARTMENTS);
-
+  const { loading, error, data } = useQuery<Product>(GET_APARTMENTS, {
+    variables: { id: productId },
+  });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error: {error.message}</p>;
 
-  const product: Product | undefined = data?.apartments.find(
-    (apt) => apt.id === productId
-  );
-  if (!product) {
+  if (!data) {
     return <p>Apartment not found!</p>;
   }
+
   return (
     <>
-      <Carousel listImages={product.pictures} />
+      <Carousel listImages={data.apartment.pictures} />
       <div className="product">
-        <h1 className="product__name">{product.title}</h1>
-        <p className="product__location">{product.location}</p>
+        <h1 className="product__name">{data.apartment.title}</h1>
+        <p className="product__location">{data.apartment.location}</p>
         <div className="product__tags">
-          {product.tags.map((tag, index) => (
+          {data.apartment.tags.map((tag, index) => (
             <p className="product-tag" key={index}>
               {tag}
             </p>
@@ -75,9 +68,9 @@ function ProductDetailsView() {
         </div>
       </div>
       <div className="product-owner">
-        <p className="product-owner__name">{product.host.name}</p>
+        <p className="product-owner__name">{data.apartment.host.name}</p>
         <div className="product-owner__photo">
-          <img src={product.host.picture} alt="Owner photo" />
+          <img src={data.apartment.host.picture} alt="Owner photo" />
         </div>
       </div>
       <div className="stars">
@@ -86,7 +79,7 @@ function ProductDetailsView() {
             key={index}
             icon={faStar}
             className={
-              index < Number(product.rating)
+              index < Number(data.apartment.rating)
                 ? "star--recieved"
                 : "star--nonrecieved"
             }
@@ -96,11 +89,11 @@ function ProductDetailsView() {
       <div className="product__descriptions">
         <Dropdown
           dropdownTitle="Description"
-          dropdownContent={product.description}
+          dropdownContent={data.apartment.description}
         />
         <Dropdown
           dropdownTitle="Équipements"
-          dropdownContent={product.equipments}
+          dropdownContent={data.apartment.equipments}
         />
       </div>
     </>
