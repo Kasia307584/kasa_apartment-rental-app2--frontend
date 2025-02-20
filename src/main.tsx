3d530fdef31425336acc428dc6c440dd8ff13c5e
Kasia307584 <63410273+Kasia307584@users.noreply.github.com>
2025-02-20 12:45:47 +0100

    styles removed from main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./styles/main.scss";
 import App from "./App.tsx";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
